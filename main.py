be7bd15a0c3a9b19625e80d522a1256737230d02
SamyBO98 <90256223+SamyBO98@users.noreply.github.com>
2026-04-25 14:10:21 +0200

    Predict culture + gradio graph works

--- main.py
+++ main.py
@@ -189,23 +189,74 @@ def pipeline_wrapper(temperature, pluviometrie, azote,
         type_sol
     )
 
-interface = gr.Interface(
-    fn=pipeline_wrapper,
-    inputs=[
-        gr.Number(label="Température"),
-        gr.Number(label="Pluviométrie"),
-        gr.Number(label="Azote"),
-        gr.Number(label="pH sol"),
-        gr.Number(label="Matière organique"),
-        gr.Number(label="Densité semis"),
-        gr.Dropdown(list(SOLS.values()), label="Type de sol")
-    ],
-    outputs=[
-        gr.Plot(label="Graphiques"),
-        gr.Textbox(label="Analyse")
-    ],
-    title="Modèle agricole : prédiction rendement"
-)
+
+def scenario_bon():
+    return 15, 600, 160, 6.7, 2.5, 210, "Limoneux"
+
+def scenario_moyen():
+    return 18, 500, 140, 6.2, 2.0, 200, "Argileux"
+
+def scenario_mauvais():
+    return 35, 200, 300, 5.0, 0.8, 400, "Sableux"
+
+def clear_inputs():
+    return None, None, None, None, None, None, None
+
+
+with gr.Blocks() as interface:
+
+    gr.Markdown("#Modèle agricole : Prédiction rendement")
+
+    with gr.Row():
+
+        # ================= LEFT SIDE (INPUTS)
+        with gr.Column(scale=1):
+
+            temp = gr.Number(label="Température")
+            pluie = gr.Number(label="Pluviométrie")
+            azote = gr.Number(label="Azote")
+            ph = gr.Number(label="pH sol")
+            org = gr.Number(label="Matière organique")
+            densite = gr.Number(label="Densité semis")
+            sol = gr.Dropdown(list(SOLS.values()), label="Type de sol")
+
+            clear_btn = gr.Button("Clear")
+            clear_btn.click(
+                fn=clear_inputs,
+                inputs=[],
+                outputs=[temp, pluie, azote, ph, org, densite, sol]
+            )
+            
+
+        # ================= RIGHT SIDE (OUTPUTS)
+        with gr.Column(scale=2):
+
+            with gr.Row():
+                gr.Button("Bon", scale=1).click(
+                    scenario_bon,
+                    outputs=[temp, pluie, azote, ph, org, densite, sol]
+                )
+
+                gr.Button("Moyen", scale=1).click(
+                    scenario_moyen,
+                    outputs=[temp, pluie, azote, ph, org, densite, sol]
+                )
+
+                gr.Button("Mauvais", scale=1).click(
+                    scenario_mauvais,
+                    outputs=[temp, pluie, azote, ph, org, densite, sol]
+                )
+            btn = gr.Button("Prédire")
+            out1 = gr.Plot(label="Graphiques")
+            out2 = gr.Textbox(label="Analyse")
+            
+
+
+    btn.click(
+        fn=pipeline_wrapper,
+        inputs=[temp, pluie, azote, ph, org, densite, sol],
+        outputs=[out1, out2]
+    )
 
 
 if __name__ == '__main__':
